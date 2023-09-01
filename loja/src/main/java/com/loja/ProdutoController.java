25919dd33cbd050024cd0ab294df3babb3bc15fd
GuilhermeTSouza <gts12ou1@gmail.com>
2023-09-01 09:24:45 -0300

    enviando atividade sobre lista de tarefas

--- loja/src/main/java/com/loja/ProdutoController.java
+++ loja/src/main/java/com/loja/ProdutoController.java
@@ -1,9 +1,11 @@
 package com.loja;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -17,5 +19,11 @@ public class ProdutoController {
     @GetMapping
     public List<Produto> buscarTodosOsProdutosDaLoja(){
         return this.tabelaProdutos.buscarTodosOsProdutos();
+    }
+
+    @GetMapping("/{produtoId}")
+    public Produto buscarProdutoPeloIdNaLoja(@PathVariable int produtoId) {
+        Produto produtoProcurado = this.tabelaProdutos.buscarProdutoPeloId(produtoId);
+        return produtoProcurado;
     }
 }
--- loja/src/main/java/com/loja/TabelaProdutos.java
+++ loja/src/main/java/com/loja/TabelaProdutos.java
@@ -1,5 +1,7 @@
 package com.loja;
 
+import ch.qos.logback.core.util.DatePatternToRegexUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +14,24 @@ public class TabelaProdutos {
         Produto lapis = new Produto(3, "Lapis Faber Castel","Lapis Faber Castel comum", 2.99);
         Produto apontador = new Produto(4, "Apontador","Apontador comum com reservatorio", 7.99);
         Produto caderno = new Produto(5, "Caderno espiral","Caderno espiral de 96 folhas", 15.99);
+        Produto pasta  = new Produto(6,"Pasta comum","Pasta comum com 50 plasticos", 29.99);
+        Produto livro = new Produto(7,"livro", "Livros diversos", 12.99);
+        Produto folhas = new Produto(8,"Folha sulfite", "Folhas sulfites brancas", 14.99);
+        Produto estojo = new Produto(9,"Estojo", "Estojos diversos com varias divisorias e personagens", 10.99);
+        Produto mochila = new Produto(10,"Mochila escolar", "Mochilas escolares diversas", 99.99);
+
 
                 this.produtos.add(caneta);
                 this.produtos.add(borracha);
                 this.produtos.add(lapis);
                 this.produtos.add(apontador);
                 this.produtos.add(caderno);
+                this.produtos.add(pasta);
+                this.produtos.add(livro);
+                this.produtos.add(folhas);
+                this.produtos.add(estojo);
+                this.produtos.add(mochila);
+
 
     }
 
@@ -27,5 +41,16 @@ public class TabelaProdutos {
 
     public List<Produto> buscarTodosOsProdutos(){
         return this.getProdutos();
+    }
+
+    public Produto buscarProdutoPeloId(int produtoId){
+        Produto produtoProcurado = null;
+        for (Produto p: this.produtos){
+            if (p.getId() == produtoId){
+                produtoProcurado = p;
+                break;
+            }
+        }
+        return produtoProcurado;
     }
 }
